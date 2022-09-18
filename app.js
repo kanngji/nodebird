93333a500c8904fca41c4d9385f7c73c420032d4
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-18 14:59:35 +0900

    node bird 기본 구조 세팅

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pageRouter = require("./routes/page");
+const { rmSync } = require("fs");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+
+// nunjucks 설정방법
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", pageRouter);
+
+// 전체 router에 에러 404
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+  error.status = 404;
+  next(error);
+});
+
+// 에러 미들웨어
+// 인자가 4개 들어가야있따 next 무조건 써야함
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
